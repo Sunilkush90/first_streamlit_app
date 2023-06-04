2a8df5691836a627ae6ba4675ab38e4084626c77
Sunil Kumar <71879438+Sunilkush90@users.noreply.github.com>
2023-06-04 13:16:50 +0530

    Update streamlit_app.py
    Menu Item

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,8 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.title('Breakfast Menu')
 
-streamlit.title('Dosa  : 50')
-streamlit.title('Idli' : 60')
-streamlit.title('Wada' : 70')
+streamlit.title('Dosa  50')
+streamlit.title('Idli' 60')
+streamlit.title('Wada' 70')
 
 
